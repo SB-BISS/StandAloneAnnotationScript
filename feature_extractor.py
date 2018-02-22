e1d0599ce689dacbb3abc222930e04ac8a6e4ce1
stefano <stefano.bromuri@ou.nl>
2018-02-22 15:37:15 +0100

    changes on the feature extractor

--- feature_extractor.py
+++ feature_extractor.py
@@ -20,25 +20,24 @@ def extract_all(filepath="./",filename="./features.csv"):
         features = np.asarray(features).reshape(len(features), -1)
         with open(filename, 'a') as f:
             w_f = csv.writer(f)
-            w_f.writerow([i.replace(filepath,"")[1:], features[0, 0], features[0, 1], features[0, 2], features[0, 3], features[0, 4],
-                          features[0, 5], features[0, 6], features[0, 7]
-                             , features[0, 8], features[0, 9], features[0, 10], features[0, 11], features[0, 12],
-                          features[0, 13], features[0, 14], features[0, 15], features[0, 16],
-                          features[0, 17], features[0, 18], features[0, 19], features[0, 20], features[0, 21],
-                          features[0, 22], features[0, 23], features[0, 24], features[0, 25],
-                          features[0, 26], features[0, 27], features[0, 28], features[0, 29], features[0, 30],
-                          features[0, 31], features[0, 32], features[0, 33]])
 
+            line_to_write = [i.replace(filepath,"")[1:]]
+            #print np.shape(features)
+            for k in range(len(features)):
+                for j in range(0,34):
+                    line_to_write.append(features[k,j])
+
+            w_f.writerow(line_to_write)
 
 def featureExtraction(file_path):
     print file_path
     [Fs, x] = audioBasicIO.readAudioFile(file_path)
-    print x
+    #print x
     features = audioFeatureExtraction.stFeatureExtraction(x, Fs, 0.05 * Fs, 0.025 * Fs)
     if (len(features) == 0 or features.shape[1] < 50):
         features = 'none'
     else:
-        features = np.mean(features, axis=1)
+        #features = np.mean(features, axis=1)
         features = np.asarray(features).reshape(len(features), -1).transpose()
 
     return features
